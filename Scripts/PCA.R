2510281d48463ad65af9db299ae41e2f120b6db6
Tom Kono <konox006@umn.edu>
2015-05-12 15:45:55 -0500

    Rename PCA script, clean it up a little bit

--- Scripts/PCA.R
+++ Scripts/PCA.R
@@ -1,9 +1,9 @@
+#   Script to perform Principal Components Analysis (PCA)
+#   This data is 50K SNP data consisting of 3764 markers that have no missing
+#   data, the first column is the PI name.
+#   Written by Michael B. Kantar
 
-#This data is 50K SNP data consisting of 3764 markers that have no missing data, the first column is the PI name
-
-Data<- read.table("file path to marker data", header=T) 
-
-
+Data <- read.table("file path to marker data", header=TRUE)
 #Make sure the data is correct (names should be chromosome positions)
 names(Data)
 
@@ -12,17 +12,16 @@ head(Data)
 length(Data[1,])
 Data[1:4,1:3]
 
-
 #Remove labels from data inorder to be able to do the PCA
-Data1 <- Data[,-(1:2)]
+Data1 <- Data[, -(1:2)]
 
 # Pricipal Components Analysis
 # entering raw data and extracting PCs 
 # from the correlation matrix 
 fit <- prcomp(Data1, cor=TRUE)
-summary(fit) # print variance accounted for 
-loadings(fit) # pc loadings 
-plot(fit,type="lines") # scree plot 
+summary(fit) # print variance accounted for
+loadings(fit) # pc loadings
+plot(fit, type="lines") # scree plot
 fit$scores # the principal components
 biplot(fit)
 
@@ -36,12 +35,19 @@ comp <- data.frame(fit$x[,1:2])
 plot(comp, pch=19, col=country)
 
 #Make Legend (Modified for cultivars addition)
-legend("bottomleft", title="Country", cex=0.75, 
-pch=16,col=c("Black","Green","Red","Blue","Cyan","Purple"), 
-legend=c("China","Japan","Cultivars","Korea","Russia", "Taiwan"), ncol=6)
+legend(
+    "bottomleft",
+    title="Country",
+    cex=0.75,
+    pch=16,
+    col=c("Black","Green","Red","Blue","Cyan","Purple"),
+    legend=c("China","Japan","Cultivars","Korea","Russia", "Taiwan"),
+    ncol=6)
 
 #load data for major cluster
-major_cluster<- read.table("C:/Users/Michael/Desktop/Forever_Green_Grant/major_cluster.txt", header=T) 
+major_cluster <- read.table(
+    "C:/Users/Michael/Desktop/Forever_Green_Grant/major_cluster.txt",
+    header=T)
 
 #Make major cluster a factor for coloring
 major <- as.factor(major_cluster[,5])
@@ -50,9 +56,14 @@ major <- as.factor(major_cluster[,5])
 plot(comp, pch=19, col=country)
 
 #Make legend
-legend("topright", title="Structure Cluster", cex=0.75, 
-pch=16,col=c("Black","Green","Red"), legend=c("cluster 1","cluster 2","cluster 3"), 
-ncol=3)
+legend(
+    "topright",
+    title="Structure Cluster",
+    cex=0.75,
+    pch=16,
+    col=c("Black","Green","Red"),
+    legend=c("cluster 1","cluster 2","cluster 3"),
+    ncol=3)
 
 # make plot of PC1 x PC2 x PC3
 comp <- data.frame(fit$x[,1:3])
@@ -65,7 +76,7 @@ plot(comp, pch=19, col=country)
 #verify PCA with new R package for PCA
 library(FactoMineR)
 result <- PCA(Data1)
-res.mca <-MCA(Data1)
+res.mca <- MCA(Data1)
 
 plot.PCA(result, axes=c(1, 2), choix="ind")
 
@@ -76,19 +87,19 @@ plotellipses(result)
 
 #######################enviromnetal pca
 #load bioclimatic & biphysical data
-dat<-read.table("file name for data", header=T)
+dat <- read.table("file name for data", header=TRUE)
 
 ## Pricipal Components Analysis
 # entering raw data and extracting PCs 
 # from the correlation matrix 
 
 #Remove labels from data inorder to be able to do the PCA
-Dat1 <- dat[,-(2:5)]
+Dat1 <- dat[, -(2:5)]
 
 fit <- prcomp(Dat1, cor=TRUE)
-summary(fit) # print variance accounted for 
-loadings(fit) # pc loadings 
-plot(fit,type="lines") # scree plot 
+summary(fit) # print variance accounted for
+loadings(fit) # pc loadings
+plot(fit, type="lines") # scree plot
 fit$scores # the principal components
 biplot(fit)
 
@@ -104,11 +115,11 @@ plot(comp, pch=19, col=major)
 library(FactoMineR)
 result <- PCA(Dat1, ncp=3) # graphs generated automatically
 plot(result)
-get_variables<-HCPC(result)
-pca_soja<-get_variables$desc.var
+get_variables <- HCPC(result)
+pca_soja <- get_variables$desc.var
 
-write.csv(pca_soja, "file path to output vaiable clusters", sep="\t",col.names=FALSE)
-
-
-
-
+write.csv(
+    pca_soja,
+    "file path to output vaiable clusters",
+    sep="\t",
+    col.names=FALSE)
